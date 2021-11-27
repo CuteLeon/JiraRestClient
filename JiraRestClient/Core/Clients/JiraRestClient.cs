8edd5e34aed034d74c8a32e6a93006836bc0c02e
Leon Liu <lliu@fnz.com>
2021-11-27 23:46:03 +0800

    Jira rest client and servise base;

---
+++ JiraRestClient/Core/Clients/JiraRestClient.cs
@@ -0,0 +1,17 @@
+﻿namespace JiraRestClient.Core.Clients;
+
+internal class JiraRestClient
+{
+    public HttpClient HttpClient { get; }
+
+    public JiraRestClient(
+        HttpClient httpClient,
+        IOptionsSnapshot<JiraRestClientOptions> options)
+    {
+        var tokenBytes = Encoding.ASCII.GetBytes($"{options.Value.UserName}:{options.Value.Password}");
+        var tokenString = Convert.ToBase64String(tokenBytes);
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", tokenString);
+        httpClient.BaseAddress = new Uri(options.Value.JiraBaseUri);
+        this.HttpClient = httpClient;
+    }
+}
---
+++ JiraRestClient/Core/Services/JiraServiceBase.cs
@@ -0,0 +1,15 @@
+﻿namespace JiraRestClient.Core.Services;
+
+internal abstract class JiraServiceBase
+{
+    public ILogger<JiraServiceBase> Logger { get; }
+    protected Clients.JiraRestClient Client { get; }
+
+    public JiraServiceBase(
+        ILogger<JiraServiceBase> logger,
+        Clients.JiraRestClient client)
+    {
+        this.Logger = logger;
+        this.Client = client;
+    }
+}
