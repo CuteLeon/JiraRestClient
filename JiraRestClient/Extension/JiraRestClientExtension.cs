aec1cb1dac7754483968ea74c96f307a0bba05f3
Leon Liu <lliu@fnz.com>
2021-11-27 23:46:10 +0800

    Extensions

---
+++ JiraRestClient/Extension/JiraRestClientExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace JiraRestClient.Extension;
+
+public static class JiraRestClientExtension
+{
+    public static IServiceCollection AddJiraRestClient(
+        this IServiceCollection services,
+        Action<JiraRestClientOptions> options)
+    => services
+        .Configure(options)
+        .AddHttpClient()
+        .AddTransient<Core.Clients.JiraRestClient>();
+}
