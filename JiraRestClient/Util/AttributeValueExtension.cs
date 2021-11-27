a7a709b707fe104f9716a768b0b6e984714250b9
Leon Liu <lliu@fnz.com>
2021-11-28 01:54:35 +0800

    Utils

---
+++ JiraRestClient/Util/AttributeValueExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace JiraRestClient.Util;
+
+internal static class AttributeValueExtension
+{
+    public static IEnumerable<object?> GetAmbientValue<TEnum>(this TEnum enumValue)
+    {
+        return typeof(TEnum)
+            .GetMember(enumValue.ToString())
+            .SelectMany(member => member.GetCustomAttributes(typeof(AmbientValueAttribute), false)
+            .Cast<AmbientValueAttribute>()
+            .Select(ambientValue => ambientValue.Value));
+    }
+}
