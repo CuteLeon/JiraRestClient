c5f6b5958cbb635c6419eeba8946d10270ee5788
Leon Liu <lliu@fnz.com>
2021-11-27 23:45:38 +0800

    Options

---
+++ JiraRestClient/Option/JiraRestClientOption.cs
@@ -0,0 +1,10 @@
+﻿namespace JiraRestClient.Option;
+
+public class JiraRestClientOptions
+{
+    public string JiraBaseUri { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Password { get; set; }
+}
